17fd4f0be7cd786bebd3380d2006c8eeaa4c08e2
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-10 16:41:41 +0200

    componenti

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-
+    <Header />
+    <Products />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Products from './components/Products.vue'
 
 
 export default {
   name: 'App',
   components: {
-   
+    Header,
+    Products,
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="d-flex align-items-center bg-dark">
+      <h1 class="text-danger">Boolflix</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style>
+
+</style>
